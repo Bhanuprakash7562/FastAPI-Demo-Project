b997bc1aa09422629531b11324f24a4fd94d9f1d
Bhanuprakash Kocharla <bhanuprakash7562@gmail.com>
2025-11-25 08:10:01 +0530

    Learning fastapi CRUD operations finished

--- Blog/main.py
+++ Blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
@@ -31,3 +31,31 @@ def get_data(db : Session = Depends(get_db)):
     q = text('select * from public."blogDetails" order by id')
     rows = db.execute(q).all()
     return [dict(row._mapping) for row in rows]
+
+@app.get("/get-blogdata/{id}")
+def get_data(id, db : Session = Depends(get_db)):
+    q = text('select * from public."blogDetails" where id = :id order by id')
+    rows = db.execute(q, {"id": id}).all() # returns list of tuples
+    print(rows)
+    if not rows:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Records not found on that {id}.")
+    # return [{"id":row[0], "Title":row[1], "Body":row[2], "Published_at":row[3]} for row in rows]
+    return [dict(row._mapping) for row in rows]
+
+@app.put("/update-data/{id}")
+def update_data(id, request : Blogdata, db : Session = Depends(get_db)):
+    q = text('update public."blogDetails" set title = :title, body = :body, "date" = :date where id = :id')
+    result = db.execute(q, {"title": request.title, "body":request.body, "date": request.published_at or datetime.utcnow(), "id" : id})
+    db.commit()
+    if result.rowcount == 0:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail=f"No rows found on the id : {id}")
+    return {"Response": f"Updated {result.rowcount} rows."}
+
+@app.delete("/delete-blog/{id}")
+def delete_blog(id, db : Session = Depends(get_db)):
+    q = text('delete from public."blogDetails" where id = :id')
+    result = db.execute(q, {"id": id})
+    db.commit()
+    if result.rowcount == 0:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail=f"{id} not found.")
+    return {"Response": f"Blog {id} is deleted."}
